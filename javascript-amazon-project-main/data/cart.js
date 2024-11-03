5023ddaa295f14571c71d61e7fbf8e32fde941bf
judith <judith.bello.torralbo@gmail.com>
2024-11-03 19:35:24 +0100

    Move addToCart into cart.js

--- javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/data/cart.js
@@ -1,1 +1,22 @@
 export const cart = [];
+
+export function addToCart(productId) {
+    //check if the product is already in the cart
+    let matchingItem;
+  
+    cart.forEach((cartItem) => {
+      if(productId === cartItem.productId) {
+        matchingItem = cartItem;
+      }
+    });
+  
+    //we add it to cart or increase the quantity
+    if(matchingItem) {
+      matchingItem.quantity++;
+    } else {
+        cart.push({
+          productId: productId,
+          quantity: 1
+        });
+    }
+  }
