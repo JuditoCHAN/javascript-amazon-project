15899297dcae025b79944c47466cc6c6ac42f438
judith <judith.bello.torralbo@gmail.com>
2024-11-09 20:10:52 +0100

    Delete products from cart when clicking delete

--- javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/data/cart.js
@@ -1,10 +1,10 @@
-export const cart = [{
+export let cart = [{
   productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
   quantity: 2
 }, {
   productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
   quantity: 1
-}]; //Normalizing the data: technique to search for all the info of the values that are in products.js
+}]; //Normalizing the data: technique to search for all the info of the values that are in products.js... mediante productId se busca el objeto en product.js para obtener las demas propiedades
 
 
 export function addToCart(productId) {
@@ -26,4 +26,17 @@ export function addToCart(productId) {
           quantity: 1
         });
     }
+  }
+
+
+  export function removeFromCart(productId) {
+    const newCart = [];
+
+    cart.forEach((cartItem) => {
+      if(cartItem.productId !== productId) {
+        newCart.push(cartItem);
+      }
+    });
+
+    cart = newCart;
   }
